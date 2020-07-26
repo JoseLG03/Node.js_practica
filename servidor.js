33e069d8dd0f98e5840c9937deff4e7df94bf08e
Jose <antares069@gmail.com>
2020-07-25 21:59:48 -0500

    pruebas

--- servidor.js
+++ servidor.js
@@ -1,9 +1,25 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(peticion, respuesta){
-    respuesta.writeHead( 200, {'Content-Type': 'text/plain'});
-    respuesta.write("Hola Nginx");    
-    respuesta.end();
+    respuesta.writeHead( 200, {'Content-Type': 'text/html'});
+    switch(peticion.url){
+        case '/':   
+            plantilla ="inicio.html";
+            break;
+        case 'nodejs':   
+            plantilla ="sobrenode.html";
+            break;    
+        default:   
+            plantilla ="404.html";
+            break;    
+    }   
+
+    fs.readFile(".plantillas/" + plantilla , function( error , datos){
+        respuesta.write(datos);
+        respuesta.end();
+    } );
+
 
 }).listen(3000, 'localhost');
 

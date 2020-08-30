b27d3e1bbc1df84db1d4e5458bb1aaffc45021c0
Jose <antares069@gmail.com>
2020-08-30 15:21:45 -0500

    valores dinamicos en PUG

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ app.use(express.static(__dirname + '/public') );
 
 app.get('/', (peticion, respuesta)=>{
     //la información por defecto sera la html de public. Si existe conflicto con el nombre del archivo
-    respuesta.render('index.pug');
+    respuesta.render('index.pug',{
+        titulo: "PlayerasJS",
+        textoParrafo:"xxx"
+    });
 })
 
 app.listen( 3000, ()=>{

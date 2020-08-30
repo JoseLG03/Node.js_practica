2c69feb47770cca5640ed1f5557d12d9fcdd0316
Jose <antares069@gmail.com>
2020-08-29 21:33:08 -0500

    servidor básico

--- servidor.js
+++ servidor.js
@@ -3,21 +3,30 @@ var fs = require('fs');
 
 http.createServer(function(peticion, respuesta){
     respuesta.writeHead( 200, {'Content-Type': 'text/html'});
+
     switch(peticion.url){
+
         case '/':   
-            plantilla ="inicio.html";
+            plantilla = "inicio.html";
             break;
-        case 'nodejs':   
-            plantilla ="sobrenode.html";
+        case '/nodejs':   
+            plantilla = "sobrenode.html";
+            break;    
+        case '/ngnix':
+            plantilla = "ngnix.html";
             break;    
         default:   
-            plantilla ="404.html";
+            plantilla = "404.html";
             break;    
     }   
 
-    fs.readFile("./plantillas/" + plantilla , function( error , datos){
-        respuesta.write(datos);
-        respuesta.end();
+    fs.readFile("./plantillas/" + plantilla , ( error , datos)=>{
+        if(error){
+            console.log("Error");
+        }else{
+            respuesta.write(datos);
+            respuesta.end();
+        }
     } );
 
 

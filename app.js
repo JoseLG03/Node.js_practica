f64df50911e14ed9d00be73e984aeb8bc848b291
Jose <antares069@gmail.com>
2020-08-30 15:56:06 -0500

    Array en nodejs

--- app.js
+++ app.js
@@ -6,11 +6,25 @@ const pug = require('pug');
 
 app.use(express.static(__dirname + '/public') );
 
+playerasArray = [
+    {color:"Naranja", imagen: "images/camiseta1.png"},
+    {color:"Roja", imagen: "images/camiseta2.png"},
+    {color:"Azul", imagen: "images/camiseta3.png"},
+    {color:"Verde", imagen: "images/camiseta4.png"}
+];
+
 app.get('/', (peticion, respuesta)=>{
     //la información por defecto sera la html de public. Si existe conflicto con el nombre del archivo
     respuesta.render('index.pug',{
         titulo: "PlayerasJS",
         textoParrafo:"xxx"
+    });
+})
+
+app.get('/tienda', (peticion, respuesta)=>{
+    //la información por defecto sera la html de public. Si existe conflicto con el nombre del archivo
+    respuesta.render('tienda.pug',{
+        playera: playerasArray
     });
 })
 
